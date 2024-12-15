4e5f4b7afeeb0c74a330a8c195b72bb5c07bbc41
Luís Fernando de Siqueira Quissak Pereira <lquissakng@gmail.com>
2024-12-15 09:17:05 -0300

    posts list

--- src/post_list.py
+++ src/post_list.py
@@ -20,5 +20,28 @@ def main(argv):
     )
     print("logged!")
 
+    users = service.users()
+    thisuser = users.get(userId="self").execute()
+    print("This user's display name is: %s" % thisuser["displayName"])
+    blogs = service.blogs()
+    thisusersblogs = blogs.listByUser(userId="self").execute()
+    for blog in thisusersblogs["items"]:
+        print("The blog named '%s' is at: %s" % (blog["name"], blog["url"]))
+
+    posts = service.posts()
+
+    with open('files/posts_list.csv', 'a') as the_file:
+        the_file.write("name|title|url\n")
+        for blog in thisusersblogs["items"]:
+            print("The posts for %s:" % blog["name"])
+            request = posts.list(blogId=blog["id"])
+            while request != None:
+                posts_doc = request.execute()
+                if "items" in posts_doc and not (posts_doc["items"] is None):
+                    for post in posts_doc["items"]:
+                        print("  %s (%s)" % (post["title"], post["url"]))
+                        the_file.write(blog["name"]+"|"+post["title"]+"|"+post["url"]+"\n")
+                request = posts.list_next(request, posts_doc)
+
 if __name__ == "__main__":
     main(sys.argv)
