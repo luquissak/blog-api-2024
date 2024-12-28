9ee6b37ebabf7d2931c61ffca345bf6e782872cf
Luís Fernando de Siqueira Quissak Pereira <lquissakng@gmail.com>
2024-12-28 09:23:33 -0300

    BQ record

---
+++ src/gcp/create_post_table.py
@@ -0,0 +1,30 @@
+from google.cloud import bigquery
+client = bigquery.Client()
+table_id = "llm-studies.blog.posts_dez_2024"
+schema = [
+    bigquery.SchemaField("blog_id", "INT64", mode="REQUIRED"),
+    bigquery.SchemaField("post_id", "INT64", mode="REQUIRED"),
+    bigquery.SchemaField("log_date", "DATETIME", mode="REQUIRED"),
+    bigquery.SchemaField("post_date", "DATE", mode="REQUIRED"),
+    bigquery.SchemaField("post_wday", "STRING", mode="NULLABLE"),
+    bigquery.SchemaField("post_url", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("post_title", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("post_content_html", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("post_content", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("post_replies", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField(
+        "post_labels",
+        "RECORD",
+        mode="REPEATED",
+        fields=[
+            bigquery.SchemaField("label", "STRING", mode="NULLABLE"),
+        ],
+    ),
+
+]
+
+table = bigquery.Table(table_id, schema=schema)
+table = client.create_table(table)
+print(
+    "Created table {}.{}.{}".format(table.project, table.dataset_id, table.table_id)
+)
