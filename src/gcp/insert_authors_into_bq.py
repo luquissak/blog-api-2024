432bf61c89f7b93ea76af871747831b61b6d661f
Luís Fernando de Siqueira Quissak Pereira <lquissakng@gmail.com>
2025-01-04 16:22:42 -0300

    fixing authors

--- src/gcp/insert_authors_into_bq.py
+++ src/gcp/insert_authors_into_bq.py
@@ -10,9 +10,7 @@ log_date = datetime.now()
 
 
 def insert_authors(authors_l, theme, area, url):
-    print("Inserting...")
-
-    print("%s (%s)..." % (url, theme))
+    print("Inserting %s..." % (url))
 
     rows_to_insert = [
         {"authors": authors_l, "theme": theme, "area": area, "url": str(url)}
@@ -20,14 +18,16 @@ def insert_authors(authors_l, theme, area, url):
 
     errors = client.insert_rows_json(table_id, rows_to_insert)
     if errors == []:
-        print("New rows have been added.")
+        return 1
     else:
         print("Encountered errors while inserting rows: {}".format(errors))
+        return 0
 
 
 def main(argv):
     print("starting!")
     with open('files/posts_authors.csv', 'r') as the_file:
+        total = 0
         for line in the_file:
             strip_line = line.rstrip()
             if strip_line == "authors|url":
@@ -36,14 +36,17 @@ def main(argv):
             authors_dict_l = aut_dict.get('authors')
             authors_l = []
             for author in authors_dict_l:
-                print(author.get('author'))
                 authors_l.append(author.get('author'))
             theme = aut_dict.get('theme')
             area = aut_dict.get('philosophical area')
+            if area == None: area = "Nenhuma"
+            if theme == None: theme = "Nenhum"
             url = strip_line.split("|")[1]
-            print(url)
-            insert_authors(authors_l, theme, area, url)
-
+            inserted = insert_authors(authors_l, theme, area, url)
+            total = total + inserted
+        return total
 
 if __name__ == "__main__":
-    main(sys.argv)
+    total = main(sys.argv)
+    print("Inseridos: %s" % total)
+
