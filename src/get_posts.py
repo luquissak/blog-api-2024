7ffd6d8895f2c057ea4055b3d54bb3eeef7b4701
Luís Quissak <lquissakng@gmail.com>
2026-02-04 16:44:53 -0300

    docao4

--- src/get_posts.py
+++ src/get_posts.py
@@ -2,6 +2,7 @@ import sys
 import os
 from dotenv import load_dotenv
 from googleapiclient import sample_tools
+import docx
 from docx import Document
 import html2text
 from datetime import datetime
@@ -11,6 +12,19 @@ load_dotenv()
 BLOG_ID = os.getenv('BLOG_ID')
 print("BLOG_ID", BLOG_ID)
 
+
+def add_hyperlink(paragraph, text, url):
+    part = paragraph.part
+    r_id = part.relate_to(url, docx.opc.constants.RELATIONSHIP_TYPE.HYPERLINK, is_external=True)
+    hyperlink = docx.oxml.shared.OxmlElement('w:hyperlink')
+    hyperlink.set(docx.oxml.shared.qn('r:id'), r_id, )
+    new_run = docx.text.run.Run(
+        docx.oxml.shared.OxmlElement('w:r'), paragraph)
+    new_run.text = text
+#    new_run.style = get_or_create_hyperlink_style(part.document)
+    hyperlink.append(new_run._element)
+    paragraph._p.append(hyperlink)
+    return hyperlink
 
 def main(argv):
     # Authenticate and construct service.
@@ -69,107 +83,109 @@ def main(argv):
                     if year == "2013":
                         document_2013.add_heading(
                         post["title"]+" - "+post_day+"\n", level=1)
-                        document_2013.add_heading(
-                        "URL: "+post["url"]+"\n", level=1)
+                        p = document_2013.add_paragraph("URL: ")
+                        add_hyperlink(p, post["title"], post["url"])
                         document_2013.add_paragraph(html2text.html2text(post["content"])+"\n")
                         document_2013.save('docao4/Reflexoes_'+str(year)+'_posts_content.docx')
                     elif year == "2014":
                         document_2014.add_heading(
                         post["title"]+" - "+post_day+"\n", level=1)
-                        document_2014.add_heading(
-                        "URL: "+post["url"]+"\n", level=1)
+                        p = document_2014.add_paragraph("URL: ")
+                        add_hyperlink(p, post["title"], post["url"])
                         document_2014.add_paragraph(html2text.html2text(post["content"])+"\n")
                         document_2014.save('docao4/Reflexoes_'+str(year)+'_posts_content.docx')
                     elif year == "2015":
                         document_2015.add_heading(
                         post["title"]+" - "+post_day+"\n", level=1)
-                        document_2015.add_heading(
-                        "URL: "+post["url"]+"\n", level=1)
+                        p = document_2015.add_paragraph("URL: ")
+                        add_hyperlink(p, post["title"], post["url"])
                         document_2015.add_paragraph(html2text.html2text(post["content"])+"\n")
                         document_2015.save('docao4/Reflexoes_'+str(year)+'_posts_content.docx')
                     elif year == "2016":
                         document_2016.add_heading(
                         post["title"]+" - "+post_day+"\n", level=1)
-                        document_2016.add_heading(
-                        "URL: "+post["url"]+"\n", level=1)
+                        p = document_2016.add_paragraph("URL: ")
+                        add_hyperlink(p, post["title"], post["url"])
                         document_2016.add_paragraph(html2text.html2text(post["content"])+"\n")
                         document_2016.save('docao4/Reflexoes_'+str(year)+'_posts_content.docx')
                     elif year == "2017":
                         document_2017.add_heading(
                         post["title"]+" - "+post_day+"\n", level=1)
-                        document_2017.add_heading(
-                        "URL: "+post["url"]+"\n", level=1)
+                        p = document_2017.add_paragraph("URL: ")
+                        add_hyperlink(p, post["title"], post["url"])
                         document_2017.add_paragraph(html2text.html2text(post["content"])+"\n")
                         document_2017.save('docao4/Reflexoes_'+str(year)+'_posts_content.docx')
                     elif year == "2018":
                         document_2018.add_heading(
                         post["title"]+" - "+post_day+"\n", level=1)
-                        document_2018.add_heading(
-                        "URL: "+post["url"]+"\n", level=1)
+                        p = document_2018.add_paragraph("URL: ")
+                        add_hyperlink(p, post["title"], post["url"])
                         document_2018.add_paragraph(html2text.html2text(post["content"])+"\n")
                         document_2018.save('docao4/Reflexoes_'+str(year)+'_posts_content.docx')
                     elif year == "2019":
                         document_2019.add_heading(
                         post["title"]+" - "+post_day+"\n", level=1)
-                        document_2019.add_heading(
-                        "URL: "+post["url"]+"\n", level=1)
+                        p = document_2019.add_paragraph("URL: ")
+                        add_hyperlink(p, post["title"], post["url"])
                         document_2019.add_paragraph(html2text.html2text(post["content"])+"\n")
                         document_2019.save('docao4/Reflexoes_'+str(year)+'_posts_content.docx')
                     elif year == "2020":
                         document_2020.add_heading(
                         post["title"]+" - "+post_day+"\n", level=1)
-                        document_2020.add_heading(
-                        "URL: "+post["url"]+"\n", level=1)
+                        p = document_2020.add_paragraph("URL: ")
+                        add_hyperlink(p, post["title"], post["url"])
                         document_2020.add_paragraph(html2text.html2text(post["content"])+"\n")
                         document_2020.save('docao4/Reflexoes_'+str(year)+'_posts_content.docx')
                     elif year == "2021":
                         document_2021.add_heading(
                         post["title"]+" - "+post_day+"\n", level=1)
-                        document_2021.add_heading(
-                        "URL: "+post["url"]+"\n", level=1)
+                        p = document_2021.add_paragraph("URL: ")
+                        add_hyperlink(p, post["title"], post["url"])
                         document_2021.add_paragraph(html2text.html2text(post["content"])+"\n")
                         document_2021.save('docao4/Reflexoes_'+str(year)+'_posts_content.docx')
                     elif year == "2022":
                         document_2022.add_heading(
                         post["title"]+" - "+post_day+"\n", level=1)
-                        document_2022.add_heading(
-                        "URL: "+post["url"]+"\n", level=1)
+                        p = document_2022.add_paragraph("URL: ")
+                        add_hyperlink(p, post["title"], post["url"])
                         document_2022.add_paragraph(html2text.html2text(post["content"])+"\n")
                         document_2022.save('docao4/Reflexoes_'+str(year)+'_posts_content.docx')
                     elif year == "2023":
                         document_2023.add_heading(
                         post["title"]+" - "+post_day+"\n", level=1)
-                        document_2023.add_heading(
-                        "URL: "+post["url"]+"\n", level=1)
+                        p = document_2023.add_paragraph("URL: ")
+                        add_hyperlink(p, post["title"], post["url"])
                         document_2023.add_paragraph(html2text.html2text(post["content"])+"\n")
                         document_2023.save('docao4/Reflexoes_'+str(year)+'_posts_content.docx')
                     elif year == "2024":
                         document_2024.add_heading(
                         post["title"]+" - "+post_day+"\n", level=1)
-                        document_2024.add_heading(
-                        "URL: "+post["url"]+"\n", level=1)
+                        p = document_2024.add_paragraph("URL: ")
+                        add_hyperlink(p, post["title"], post["url"])
                         document_2024.add_paragraph(html2text.html2text(post["content"])+"\n")
                         document_2024.save('docao4/Reflexoes_'+str(year)+'_posts_content.docx')
                     elif year == "2025":
                         document_2025.add_heading(
                         post["title"]+" - "+post_day+"\n", level=1)
+                        p = document_2025.add_paragraph("URL: ")
+                        add_hyperlink(p, post["title"], post["url"])
                         document_2025.add_paragraph(html2text.html2text(post["content"])+"\n")
                         document_2025.save('docao4/Reflexoes_'+str(year)+'_posts_content.docx')
                     elif year == "2026":
                         document_2026.add_heading(
                         post["title"]+" - "+post_day+"\n", level=1)
-                        document_2026.add_heading(
-                        "URL: "+post["url"]+"\n", level=1)
+                        p = document_2026.add_paragraph("URL: ")
+                        add_hyperlink(p, post["title"], post["url"])
                         document_2026.add_paragraph(html2text.html2text(post["content"])+"\n")
                         document_2026.save('docao4/Reflexoes_'+str(year)+'_posts_content.docx')
                     else:
                         print("DEU RUIM")
 #                    continue
-                    #if i > 3: exit()
+                    if i > 3: exit()
                     document.add_heading(
                        post["title"]+" - "+post_day+"\n", level=1)
-                    document.add_heading(
-                        "URL: "+post["url"]+"\n", level=1)
+                    p = document.add_paragraph("URL: ")
+                    add_hyperlink(p, post["title"], post["url"])
                     document.add_paragraph(html2text.html2text(post["content"])+"\n")
                     document.save('docao4/Reflexoes_posts_content.docx')
                     i = i + 1
